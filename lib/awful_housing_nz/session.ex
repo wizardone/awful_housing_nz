a4b502419bd9aa11f5ae5e59b3ca2b73caca43c6
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2018-11-26 21:30:08 +1300

    some wip on session changesets

--- lib/awful_housing_nz/session.ex
+++ lib/awful_housing_nz/session.ex
@@ -7,7 +7,24 @@ defmodule AwfulHousingNz.Session do
   @doc false
   def changeset(attrs) do
     {%AwfulHousingNz.Session{}, %{email: :string, password: :string}}
-      |> Ecto.Changeset.cast(attrs, @attributes)
-      |> Ecto.Changeset.validate_required(@attributes)
+    |> Ecto.Changeset.cast(attrs, @attributes)
+    |> Ecto.Changeset.validate_required(@attributes)
+    |> validate_password
+  end
+
+  def authenticate(%AwfulHousingNz.Session{email: email, password: password}) do
+    
+  end
+
+  defp validate_password(changeset) do
+    case changeset.changes do
+      %{email: email, password: password} ->
+        user = AwfulHousingNz.Repo.get_by(AwfulHousingNz.User, email: email)
+        case user do
+          %AwfulHousingNz.User{encrypted_password: encrypted_password} -> AwfulHousingNz.User.password_correct?(user, password)
+          nil -> add_error(changeset, :email, "No user with this email")
+        end
+      %{} -> changeset
+    end
   end
 end
--- lib/awful_housing_nz_web/controllers/session_controller.ex
+++ lib/awful_housing_nz_web/controllers/session_controller.ex
@@ -7,8 +7,9 @@ defmodule AwfulHousingNzWeb.SessionController do
   end
 
   def create(conn, %{"session" => session_params}) do
-    changeset = %{Session.changeset(session_params) | action: :insert}
+    changeset = Session.changeset(session_params)
     case changeset.valid? do
+      # TODO: Maybe puth Session.authenticate here?
       true -> conn |> put_flash(:info, "Logged in successfully") |> redirect(to: page_path(conn, :index))
       false -> conn |> render("new.html", changeset: changeset)
     end
